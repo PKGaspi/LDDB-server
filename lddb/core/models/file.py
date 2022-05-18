ba6b96bf67cfa3ba3096c82fb4490add8712d783
Juan Gaspar García <24842781+PKGaspi@users.noreply.github.com>
2022-05-18 23:52:58 +0800

    Deleted File model and schema

--- lddb/core/models/file.py
+++
@@ -1,9 +0,0 @@
-import uuid
-from sqlalchemy import Column, String
-
-from lddb.core.database import Base
-
-class File(Base):
-    __tablename__ = "files"
-
-    filename = Column(String, unique=True, nullable=False)
